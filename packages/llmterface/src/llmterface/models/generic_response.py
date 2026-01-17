a7fdaeb1a44cadb8d5994da53c1dc0300075d5eb
D. Zachary Wheeler <celestialswashbuckler@gmail.com>
2026-01-17 18:17:59 -0500

    remove unused hash func

--- packages/llmterface/src/llmterface/models/generic_response.py
+++ packages/llmterface/src/llmterface/models/generic_response.py
@@ -9,8 +9,3 @@ class GenericResponse(t.Generic[TOrig]):
     original: TOrig
     text: str
     metadata: t.Mapping[str, t.Any] = field(default_factory=dict)
-
-    def get_hash(self) -> int:
-        return hash(self.text)
-
-    __hash__ = get_hash
